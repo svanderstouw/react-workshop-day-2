c1b92e28879347b5db18f2480b0faea125f4ac0f
Steve Vander Stouw <svanderstouw@gmail.com>
2018-12-19 13:17:25 -0500

    Changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-const Button = props => {
+const Button = ({color, behavior, textContent}) => {
     const buttonStyle = {
-        backgroundColor: props.color
+        backgroundColor: color
     }
-    return <button style={buttonStyle} onClick={props.behavior}>{props.textContent}</button>
+    return <button 
+                style={buttonStyle} 
+                onClick={behavior}
+                >{textContent}</button>
 }
 
 export default Button
